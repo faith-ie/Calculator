0ac2f1f73d34040a71edfffb0b4e4a8dc8b732c9
Daniel Viola <daniel.viola001@mymdc.net>
2023-07-22 13:08:19 -0400

    Made some changes, i have to fix the TryParse() at some point

--- Program.cs
+++ Program.cs
@@ -2,61 +2,65 @@
 
 namespace Calculator
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("**Welcome to my calulator!**");
-            Console.WriteLine("Please enter the number of the option you would like to do : \n1. Subtract\n2. Add\n3. Multiply\n4. Divide\n5. Exit");
+        private static void Main() => new Program().Result();
 
-            int Choice = int.Parse(Console.ReadLine());
-            switch (Choice)
+        private int num1, num2;
+
+        private async void Result()
+        {
+            Console.WriteLine("***Welcome to my calculator!***");
+            Console.WriteLine($"Please enter the number of the operation you would like to do: \n{Operation.Add}\n{Operation.Subtract}\n{Operation.Multiply}\n{Operation.Divide}");
+            int result = 0;
+            if (Enum.TryParse<Operation>(Console.ReadLine(), out var op))
             {
-                case 1:
-                    Console.WriteLine("Please enter the first number to subtract : \n");
-                    int x = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter the second number to subtract : \n");
-                    int y = int.Parse(Console.ReadLine());
-                    int ans = x - y;
-                    Console.WriteLine($"If we subtract those two numbers, we get {ans}.");
-                    Console.WriteLine("Press any key to exit...");
-                    Console.ReadKey();
-                    break;
-                case 2:
-                    Console.WriteLine("Please enter the first number to add : \n");
-                    int a = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter the second number to add : \n");
-                    int b = int.Parse(Console.ReadLine());
-                    int ans2 = a + b;
-                    Console.WriteLine($"If we add those two numbers we get {ans2}.");
-                    Console.WriteLine("Press any key to exit...");
-                    Console.ReadKey();
-                    break;
-                case 3:
-                    Console.WriteLine("Please enter the first number to multiply : \n");
-                    int c = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter the second number to multiply : \n");
-                    int d = int.Parse(Console.ReadLine());
-                    int ans3 = c * d;
-                    Console.WriteLine($"If we multiply those two numbers we get {ans3}.");
-                    Console.WriteLine("Press any key to exit...");
-                    Console.ReadKey();
-                    break;
-                case 4:
-                    Console.WriteLine("Please enter the first number to divide : \n");
-                    int e = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Please enter the second number to divide : \n");
-                    int f = int.Parse(Console.ReadLine());
-                    int ans4 = e / f;
-                    Console.WriteLine($"If we multiply those two numbers we get {ans4}.");
-                    Console.WriteLine("Press any key to exit...");
-                    Console.ReadKey();
-                    break;
-                case 5:
-                    Console.WriteLine("See you next time!");
-                    System.Environment.Exit(0);
-                    break;
+                switch (op)
+                {
+                    case Operation.Add:
+                        Console.WriteLine($"Insert two numbers to {op} now.");
+                        int.TryParse(Console.ReadLine(), out num1);
+                        int.TryParse(Console.ReadLine(), out num2);
+                        result = num1 + num2;
+                        break;
+
+                    case Operation.Subtract:
+                        Console.WriteLine($"Insert two numbers to {op} now.");
+                        int.TryParse(Console.ReadLine(), out num1);
+                        int.TryParse(Console.ReadLine(), out num2);
+                        result = num1 - num2;
+                        break;
+
+                    case Operation.Multiply:
+                        Console.WriteLine($"Insert two numbers to {op} now.");
+                        int.TryParse(Console.ReadLine(), out num1);
+                        int.TryParse(Console.ReadLine(), out num2);
+                        result = num1 * num2;
+                        break;
+
+                    case Operation.Divide:
+                        Console.WriteLine($"Insert two numbers to {op} now.");
+                        int.TryParse(Console.ReadLine(), out num1);
+                        int.TryParse(Console.ReadLine(), out num2);
+                        result = num1 / num2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid operation. Please try again.");
+                        break;
+                }
             }
+            await Console.Out.WriteLineAsync($"If we {op} these two numbers, we get {result}");
+            await Console.Out.WriteLineAsync("Press any key to exit.");
+            Console.ReadKey();
         }
+
+        private enum Operation
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        };
     }
 }
